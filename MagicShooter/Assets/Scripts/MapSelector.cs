e11410b8708ccfc8b3f245e42a783d7e19f51fcd
Andrey Lisin <andrey.lisin11@gmail.com>
2024-02-16 12:58:47 +0500

    FixLighting

--- MagicShooter/Assets/Scripts/MapSelector.cs
+++ MagicShooter/Assets/Scripts/MapSelector.cs
@@ -16,6 +16,7 @@ public class MapSelector : MonoBehaviour
         public TerrainData _TerrainData;
         public VolumeProfile _VolumeProfile;
         public Material _Skybox;
+        public Color _AmbientColor;
     }
 
     public void SelectMap(int i)
@@ -25,47 +26,7 @@ public class MapSelector : MonoBehaviour
         _terrain.terrainData = Presets[i]._TerrainData;
         _volume.profile = Presets[i]._VolumeProfile;
         RenderSettings.skybox = Presets[i]._Skybox;
+        RenderSettings.ambientLight = Presets[i]._AmbientColor;
         SaveManager.Instance.CurrentProgress.SelectedMap = i;
-    }
-
-    [ContextMenu("SelectMeadow")]
-    public void SelectMeadow()
-    {
-        _camera.transform.position = Presets[0]._CameraTransform.position;
-        _camera.transform.rotation = Presets[0]._CameraTransform.rotation;
-        _terrain.terrainData = Presets[0]._TerrainData;
-        _volume.profile = Presets[0]._VolumeProfile;
-        RenderSettings.skybox = Presets[0]._Skybox;
-        SaveManager.Instance.CurrentProgress.SelectedMap = 0;
-    }
-    [ContextMenu("SelectDesert")]
-    public void SelectDesert()
-    {
-        _camera.transform.position = Presets[1]._CameraTransform.position;
-        _camera.transform.rotation = Presets[1]._CameraTransform.rotation;
-        _terrain.terrainData = Presets[1]._TerrainData;
-        _volume.profile = Presets[1]._VolumeProfile;
-        RenderSettings.skybox = Presets[1]._Skybox;
-        SaveManager.Instance.CurrentProgress.SelectedMap = 1;
-    }
-    [ContextMenu("SelectArctic")]
-    public void SelectArctic()
-    {
-        _camera.transform.position = Presets[2]._CameraTransform.position;
-        _camera.transform.rotation = Presets[2]._CameraTransform.rotation;
-        _terrain.terrainData = Presets[2]._TerrainData;
-        _volume.profile = Presets[2]._VolumeProfile;
-        RenderSettings.skybox = Presets[2]._Skybox;
-        SaveManager.Instance.CurrentProgress.SelectedMap = 2;
-    }
-    [ContextMenu("SelectMoon")]
-    public void SelectMoon()
-    {
-        _camera.transform.position = Presets[3]._CameraTransform.position;
-        _camera.transform.rotation = Presets[3]._CameraTransform.rotation;
-        _terrain.terrainData = Presets[3]._TerrainData;
-        _volume.profile = Presets[3]._VolumeProfile;
-        RenderSettings.skybox = Presets[3]._Skybox;
-        SaveManager.Instance.CurrentProgress.SelectedMap = 3;
     }
 }
